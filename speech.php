78dd3217a8bceef433cbd5be6243ab7ee90099c7
HsiaoFeng <vectorst@foxmail.com>
2022-10-12 23:35:51 +0800

    修正显示问题。

--- speech.php
+++ speech.php
@@ -6,7 +6,7 @@
 	while($row = mysqli_fetch_array($result)){
 		echo "<div class=\"speech mb-4\"><p>".$row['content'] .
 		"</p><br/><p class=\"info\">#".$row['id'].
-		"&nbsp;<strong>".$row['name']."</strong>&nbsp;来自<strong>".$row['class']."</strong>&nbsp;<span style=\"color:#b9afaf;\">".$row['dateandtime']."</span></p></div>";
+		"&nbsp;<strong>".$row['name']."</strong>&nbsp;".$row['class']."&nbsp;<span style=\"color:#b9afaf;\">".$row['dateandtime']."</span></p></div>";
 	}
 	mysqli_close($con);
 ?>
